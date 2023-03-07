08e67ac6c54c1e53987c3f53355c54d40c776acb
valarmathi <valarkannan@gmail.com>
2023-03-07 14:56:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,7 +137,14 @@ resource "aws_instance" "webserver1" {
   availability_zone      = "${var.region}a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
-  user_data              = file("install_apache.sh")
+  user_data              = <<EOF
+  #! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+EOF
 
   tags = {
     App = var.stack
@@ -152,7 +159,14 @@ resource "aws_instance" "webserver2" {
   availability_zone      = "${var.region}b"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-2.id
-  user_data              = file("install_apache.sh")
+  user_data              = <<EOF
+  #! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+EOF
 
   tags = {
     App = var.stack
